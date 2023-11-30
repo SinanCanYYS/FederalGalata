a33d175447bf4ac93366567f4d58b8de880d0916
SinanC <125397960+SinanCanYYS@users.noreply.github.com>
2023-11-30 16:22:52 +0000

    Setup mongoose and MongoDB connection

---
+++ src/database-connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/FederalStock')
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(err => console.log('Error to Connecting MongoDB', err))
+
+// const Cat = mongoose.model('Cat', { name: String })
+
+// const kitty = new Cat({ name: 'Waldachtal' })
+// kitty.save().then(() => console.log('meow'))
