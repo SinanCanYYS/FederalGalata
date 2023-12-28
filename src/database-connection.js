a552322fb4a28450db93e9ea87aa8d4282f18077
SinanC <125397960+SinanCanYYS@users.noreply.github.com>
2023-12-28 12:35:07 +0000

    Finalize Docker settings

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose')
 mongoose
+  // .connect('mongodb://127.0.0.1:27017/FederalStock')
+  // .connect('mongodb://192.168.2.112:27017/FederalStockTest')
   .connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/FederalStock')
   .then(() => {
     console.log('Connected to MongoDB')
   })
   .catch(err => console.log('Error to Connecting MongoDB', err))
-
-// const Cat = mongoose.model('Cat', { name: String })
-
-// const kitty = new Cat({ name: 'Waldachtal' })
-// kitty.save().then(() => console.log('meow'))
